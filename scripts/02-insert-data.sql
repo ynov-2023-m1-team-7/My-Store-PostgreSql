15059fca1310369262029b3520ccdf7855703bb6
DamienDrozd <damien.drozd@icloud.com>
2023-12-21 13:18:47 +0100

    new images link

--- scripts/02-insert-data.sql
+++ scripts/02-insert-data.sql
@@ -4,8 +4,8 @@ VALUES (
         'W simple pant',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product1.webp',
-        '/uploads/product1_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product1_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product1_packshot.webp',
         123
     ),
     (
@@ -13,8 +13,8 @@ VALUES (
         'Bib Overall Straight',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product2.webp',
-        '/uploads/product2_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product2_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product2_packshot.webp',
         136
     ),
     (
@@ -22,8 +22,8 @@ VALUES (
         'W'' L/S Kyle Shirt',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product3.jpeg',
-        '/uploads/product3_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product3_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product3_packshot.webp',
         89.499999999999999998
     ),
     (
@@ -31,8 +31,8 @@ VALUES (
         'W'' American Scr. High Neck Swt',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product4.jpeg',
-        '/uploads/product4_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product4_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product4_packshot.webp',
         120.98999999999999488
     ),
     (
@@ -40,8 +40,8 @@ VALUES (
         'W'' L/S Seidler T-Shir',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product5.jpeg',
-        '/uploads/product5_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product5_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product5_packshot.webp',
         70
     ),
     (
@@ -49,8 +49,8 @@ VALUES (
         'W'' OG Active Jacket',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product6.jpeg',
-        '/uploads/product6_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product6_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product6_packshot.webp',
         90
     ),
     (
@@ -58,8 +58,8 @@ VALUES (
         'W'' Ace Vest',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product7.webp',
-        '/uploads/product7_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product7_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product7_packshot.webp',
         78
     ),
     (
@@ -67,8 +67,8 @@ VALUES (
         'W'' L/S Craft Shirt ',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product8.webp',
-        '/uploads/product8_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product8_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product8_packshot.webp',
         90
     ),
     (
@@ -76,7 +76,7 @@ VALUES (
         'W'' Skyler Liner',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
         true,
-        'uploads/product9.jpeg',
-        '/uploads/product9_packshot.jpeg',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product9_packshot.webp',
+        'https://my-store-files.s3.eu-west-3.amazonaws.com//uploads/product9_packshot.webp',
         370.49999999999999999
     );
